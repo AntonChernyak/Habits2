915a2b902c9554a8b2d7a138f941221b8c9e04b6
AntonChernyak <anton767@yandex.ru>
2022-02-17 15:06:41 +0300

    Added logging of the first activity.

--- app/src/main/java/com/example/habits/FirstActivity.kt
+++ app/src/main/java/com/example/habits/FirstActivity.kt
@@ -2,6 +2,7 @@ package com.example.habits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.habits.databinding.ActivityMainBinding
 
@@ -12,8 +13,34 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(FIRST_ACTIVITY_TAG, "onCreate()")
 
         viewBinding.counterTextView.text = "0"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(FIRST_ACTIVITY_TAG, "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(FIRST_ACTIVITY_TAG, "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(FIRST_ACTIVITY_TAG, "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(FIRST_ACTIVITY_TAG, "onStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(FIRST_ACTIVITY_TAG, "onDestroy()")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -27,7 +54,13 @@ class FirstActivity : AppCompatActivity() {
         viewBinding.counterTextView.text = restoreNumber.toString()
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(FIRST_ACTIVITY_TAG, "onRestart()")
+    }
+
     companion object {
         private const val COUNTER_KEY = "counter key"
+        private const val FIRST_ACTIVITY_TAG = "first activity tag"
     }
 }
