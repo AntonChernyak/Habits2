f4db8300f0f012bcd905dde3f01793b1c4e1482e
AntonChernyak <anton767@yandex.ru>
2022-03-24 14:50:27 +0300

    added navigation drawer layout

--- app/src/main/java/com/example/habits/activity/MainActivity.kt
+++ app/src/main/java/com/example/habits/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.habits.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.habits.R
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
