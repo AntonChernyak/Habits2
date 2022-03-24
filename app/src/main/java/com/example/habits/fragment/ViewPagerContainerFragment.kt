df703537994d852df32a39db5d6b13f001ca59b0
AntonChernyak <anton767@yandex.ru>
2022-03-24 16:37:24 +0300

    created ViewPagerContainer code file

--- app/src/main/java/com/example/habits/fragment/ViewPagerContainerFragment.kt
+++ app/src/main/java/com/example/habits/fragment/ViewPagerContainerFragment.kt
@@ -5,56 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.habits.R
+import com.example.habits.adapter.HabitViewPagerAdapter
+import com.example.habits.databinding.FragmentViewPagerContainerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ViewPagerContainerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ViewPagerContainerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding: FragmentViewPagerContainerBinding by viewBinding()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_pager_container, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ViewPagerContainerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ViewPagerContainerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.habitsViewPager.apply {
+            adapter = HabitViewPagerAdapter(this@ViewPagerContainerFragment, requireActivity())
+            orientation = ViewPager2.ORIENTATION_HORIZONTAL
+            val child = getChildAt(0)
+            if (child is RecyclerView) {
+                child.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
             }
+        }
+
+        TabLayoutMediator(binding.habitsTabLayout, binding.habitsViewPager) { tab, position ->
+            when (position) {
+                POSITIVE_TAB_POSITION -> tab.text = getString(R.string.goods)
+                NEGATIVE_TAB_POSITION -> tab.text = getString(R.string.bads)
+            }
+        }.attach()
+    }
+
+    companion object {
+        const val POSITIVE_TAB_POSITION = 0
+        const val NEGATIVE_TAB_POSITION = 1
     }
 }
