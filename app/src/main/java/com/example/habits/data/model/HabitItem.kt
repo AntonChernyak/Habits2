acc70654c9ed5edf29347b93f54f9235b74ae162
AntonChernyak <anton767@yandex.ru>
2022-04-14 00:58:24 +0300

    created Entity class

--- app/src/main/java/com/example/habits/data/model/HabitItem.kt
+++ app/src/main/java/com/example/habits/data/model/HabitItem.kt
@@ -2,19 +2,30 @@ package com.example.habits.data.model
 
 import android.graphics.Color
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Entity(tableName = "habit_db")
 @Parcelize
 data class HabitItem(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = -1,
     val title: String,
     val description: String = "",
     val priority: String,
+    @Embedded
     val type: HabitType = HabitType.GOOD_HABIT,
+    @ColumnInfo(name = "period_count")
     val periodCount: String,
+    @ColumnInfo(name = "period_days")
     val periodDays: String,
     val color: Int = Color.WHITE,
+    @ColumnInfo(name = "date_of_creation")
     val dateOfCreation: Long = Date().time,
+    @ColumnInfo(name = "is_checked")
     var isChecked: Boolean = false
 ): Parcelable
