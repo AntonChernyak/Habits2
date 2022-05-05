cdcf6f8f51785abc9b6e9c6943a34ec1f17b61c1
AntonChernyak <anton767@yandex.ru>
2022-05-05 14:58:11 +0300

    added kotlinx-serialization

--- app/src/main/java/com/example/habits/data/model_dto/ErrorDto.kt
+++ app/src/main/java/com/example/habits/data/model_dto/ErrorDto.kt
@@ -1,5 +1,8 @@
 package com.example.habits.data.model_dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class ErrorDto(
     val code: Int,
     val message: String
--- app/src/main/java/com/example/habits/data/model_dto/HabitDto.kt
+++ app/src/main/java/com/example/habits/data/model_dto/HabitDto.kt
@@ -1,11 +1,16 @@
 package com.example.habits.data.model_dto
 
-class Habit(
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+class HabitDto(
     val color: Int,
     val count: Int,
     val date: Int,
     val description: String,
-    val done_dates: IntArray,
+    @SerialName("done_dates")
+    val doneDates: IntArray,
     val frequency: Int,
     val priority: IntArray,
     val title: String,
