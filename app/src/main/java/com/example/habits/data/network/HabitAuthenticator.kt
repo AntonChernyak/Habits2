100e96701c333e7fe2507c80009b4b0de0ecef98
AntonChernyak <anton767@yandex.ru>
2022-05-12 13:35:13 +0300

    added Glide dependencies and changed avatar container to ShapeableImageView

--- app/src/main/java/com/example/habits/data/network/HabitAuthenticator.kt
+++ app/src/main/java/com/example/habits/data/network/HabitAuthenticator.kt
@@ -5,7 +5,7 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
 
-class HabitAuthenticator() : Authenticator {
+class HabitAuthenticator : Authenticator {
 
     override fun authenticate(route: Route?, response: Response): Request {
 
