4982e5d093d1c8e378ba82d64f72e424f16b2acf
Anton Chernyak <eldariliona@gmail.com>
2023-02-17 21:44:14 +0300

    added logging to FirstActivity.kt

--- app/src/main/java/com/antoncherniak/habits/FirstActivity.kt
+++ app/src/main/java/com/antoncherniak/habits/FirstActivity.kt
@@ -5,42 +5,42 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 
-class SecondActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
-        Log.d(SECOND_ACTIVITY_TAG, "onCreate()_2")
+        setContentView(R.layout.activity_main)
+        Log.d(FIRST_ACTIVITY_TAG, "onCreate()_1")
 
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(SECOND_ACTIVITY_TAG, "onStart()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onStart()_1")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(SECOND_ACTIVITY_TAG, "onResume()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onResume()_1")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(SECOND_ACTIVITY_TAG, "onPause()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onPause()_1")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(SECOND_ACTIVITY_TAG, "onStop()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onStop()_1")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(SECOND_ACTIVITY_TAG, "onDestroy()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onDestroy()_1")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        Log.d(SECOND_ACTIVITY_TAG, "onSaveInstanceState()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onSaveInstanceState()_1")
     }
 
     override fun onRestoreInstanceState(
@@ -48,16 +48,16 @@ class SecondActivity : AppCompatActivity() {
         persistentState: PersistableBundle?
     ) {
         super.onRestoreInstanceState(savedInstanceState, persistentState)
-        Log.d(SECOND_ACTIVITY_TAG, "onRestoreInstanceState()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onRestoreInstanceState()_1")
 
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d(SECOND_ACTIVITY_TAG, "onRestart()_2")
+        Log.d(FIRST_ACTIVITY_TAG, "onRestart()_1")
     }
 
     companion object{
-        private const val SECOND_ACTIVITY_TAG = "second activity"
+        private const val FIRST_ACTIVITY_TAG = "first activity"
     }
 }
