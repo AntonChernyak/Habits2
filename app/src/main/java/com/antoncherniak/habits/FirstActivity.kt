8bab829a6b33eaae4808354431772b69c424091d
Anton Chernyak <eldariliona@gmail.com>
2023-02-17 22:57:35 +0300

    added logic to increase the number on the first activity.

--- app/src/main/java/com/antoncherniak/habits/FirstActivity.kt
+++ app/src/main/java/com/antoncherniak/habits/FirstActivity.kt
@@ -2,14 +2,21 @@ package com.antoncherniak.habits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.antoncherniak.habits.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    private val binding: ActivityFirstBinding by viewBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
         Log.d(FIRST_ACTIVITY_TAG, "onCreate()_1")
+
+        if (savedInstanceState == null) {
+            binding.firstActivityNumberTextView.text = DEFAULT_COUNTER_VALUE
+        }
 
     }
 
@@ -41,15 +48,22 @@ class FirstActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Log.d(FIRST_ACTIVITY_TAG, "onSaveInstanceState()_1")
+
+        val saveValue = binding.firstActivityNumberTextView
+            .text
+            .toString()
+            .toInt()
+            .plus(1)
+
+        outState.putInt(COUNTER_KEY, saveValue)
     }
 
-    override fun onRestoreInstanceState(
-        savedInstanceState: Bundle?,
-        persistentState: PersistableBundle?
-    ) {
-        super.onRestoreInstanceState(savedInstanceState, persistentState)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
         Log.d(FIRST_ACTIVITY_TAG, "onRestoreInstanceState()_1")
 
+        val restoreValue = savedInstanceState.getInt(COUNTER_KEY, DEFAULT_COUNTER_VALUE.toInt())
+        binding.firstActivityNumberTextView.text = restoreValue.toString()
     }
 
     override fun onRestart() {
@@ -57,7 +71,9 @@ class FirstActivity : AppCompatActivity() {
         Log.d(FIRST_ACTIVITY_TAG, "onRestart()_1")
     }
 
-    companion object{
+    companion object {
         private const val FIRST_ACTIVITY_TAG = "first activity"
+        private const val COUNTER_KEY = "key counter"
+        private const val DEFAULT_COUNTER_VALUE = "0"
     }
 }
